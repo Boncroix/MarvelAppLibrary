d477fdaf8a20371de93db749c01c0bfb9f6617d7
Boncroix <boncroix@gmail.com>
2024-06-04 07:23:55 +0200

    Corregir archivo estrutura de la libreria

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "MarvelAppLibrary",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v10_15)
-    ],
+                .iOS(.v16),
+                .macOS(.v10_15)
+               ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,7 +16,8 @@ let package = Package(
             targets: ["MarvelAppLibrary"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "20.0.0"))
+        .package(url: "https://github.com/evgenyneu/keychain-swift", .upToNextMajor(from: "20.0.0"))
+    
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,7 +26,9 @@ let package = Package(
             name: "MarvelAppLibrary",
             dependencies: [
                 .product(name: "KeychainSwift", package: "keychain-swift")
-            ]),
+            ]
+        ),
+        
         .testTarget(
             name: "MarvelAppLibraryTests",
             dependencies: ["MarvelAppLibrary"]),
