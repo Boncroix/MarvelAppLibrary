60978c6fadd6bec406315023c8d27635c1dd112d
Boncroix <boncroix@gmail.com>
2024-06-04 19:11:05 +0200

    Añadir estilos de fuente

--- Sources/MarvelAppLibrary/UI/AppFonts/AppFonts.swift
+++ Sources/MarvelAppLibrary/UI/AppFonts/AppFonts.swift
@@ -15,14 +15,26 @@ public struct AppFonts{
     
     // MARK: Properties
     public var textXL = Font.custom("SF PRO", size: 40)
+    public var textXLBold = Font.custom("SF PRO", size: 40)
+        .bold()
     
     public var textL = Font.custom("SF PRO", size: 32)
+    public var textLBold = Font.custom("SF PRO", size: 32)
+        .bold()
     
     public var textM = Font.custom("SF PRO", size: 28)
+    public var textMBold = Font.custom("SF PRO", size: 28)
+        .bold()
     
     public var textS = Font.custom("SF PRO", size: 24)
+    public var textSBold = Font.custom("SF PRO", size: 24)
+        .bold()
     
     public var textXS = Font.custom("SF PRO", size: 16)
+    public var textXSBold = Font.custom("SF PRO", size: 16)
+        .bold()
     
     public var textXSs = Font.custom("SF PRO", size: 8)
+    public var textXSsBold = Font.custom("SF PRO", size: 8)
+        .bold()
 }
